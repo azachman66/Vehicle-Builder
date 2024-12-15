650a293012265059c13f7df06b590efdea510281
Aspen Zachman <azachman66@gmail.com>
2024-12-15 18:00:35 -0500

    Update Driveable.js

--- interfaces/Driveable.js
+++ interfaces/Driveable.js
@@ -1,1 +1,16 @@
-export {};
+// definition of the Driveable interface
+interface Driveable {
+  // declare the properties
+  started: boolean;
+  currentSpeed: number;
+  // declare the methods
+  start(): void;
+  accelerate(change: number): void;
+  decelerate(change: number): void;
+  stop(): void;
+  turn(direction: string): void;
+  reverse(): void;
+}
+
+// export the Driveable interface
+export default Driveable;
